aa896416ef474f8a19df7366448ee9b3f991bc90
Rodrigo Farinango <rodrigofarii@gmail.com>
2025-08-18 16:27:39 -0600

    Add desestructuracion arrays

---
+++ 01-bases/src/bases/06-deses-arr.ts
@@ -0,0 +1,13 @@
+const character = ['Goku', 'Vegeta', 'Trunks', 'Goten'];
+
+const [ g,v,t, g2, g3= 'Yamoshi' ] = character;
+
+console.log({ g,v,t, g2, g3 });
+
+const returnArray = () => {
+    return [123, 'ABC'] as const;
+}
+
+const [ numbers, letters ] = returnArray();
+
+console.log(numbers * 2, letters.toLowerCase());
