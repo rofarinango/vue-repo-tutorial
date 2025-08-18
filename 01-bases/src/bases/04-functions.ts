967b873b2ae31293037d7ee6773cac68f4f699e1
Rodrigo Farinango <rodrigofarii@gmail.com>
2025-08-18 16:05:26 -0600

    Add functions part 2 lesson

--- 01-bases/src/bases/04-functions.ts
+++ 01-bases/src/bases/04-functions.ts
@@ -16,4 +16,22 @@ const getUser = (uid: string) =>
         username: 'Tony001',
     }) // Cuando se usa parentesis se refiere a un return implicito
 
+console.log(greetPerson('Tony'));
+
 console.log(getUser('XYZ-456'));
+
+const heroes = [
+    {
+        id: 1,
+        name: 'Batman',
+    },
+    {
+        id: 2,
+        name: 'Superman',
+        power: 'Super fuerza',
+    },
+];
+
+const hero = heroes.find( (h) => h.id === 1 );
+
+console.log(hero?.power?.toUpperCase());
--- 01-bases/src/main.ts
+++ 01-bases/src/main.ts
@@ -5,7 +5,7 @@ import viteLogo from '/vite.svg';
 //import './bases/01-const-let';
 //import './bases/02-object';
 //import './bases/03-arrays';
-import './bases/04-functions'
+//import './bases/04-functions'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
