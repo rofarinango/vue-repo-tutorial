6a42f83b73050e49a23b819c6982449c6776d654
Rodrigo Farinango <rodrigofarii@gmail.com>
2025-08-19 11:15:16 -0600

    Add typescript interfaces

--- 01-bases/src/bases/09-fetch-api.ts
+++ 01-bases/src/bases/09-fetch-api.ts
@@ -1,6 +1,10 @@
+import type { GIFResponse } from "../interfaces/gif.response";
+
 const apiKey = 'Z73kEisIPtPkApP4h16UKltuFDm6828Q'
 
 fetch(`https://api.giphy.com/v1/gifs/random?api_key=${ apiKey }`)
     .then(resp => resp.json())
-    .then(({data}) => console.log({data}))
+    .then((body: GIFResponse) => {
+        console.log(body.data.images.downsized_medium.url);   
+    })
     .catch(err => console.warn(err));
