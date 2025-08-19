8a94286182806624467a686c07de6ea4b4a567c8
Rodrigo Farinango <rodrigofarii@gmail.com>
2025-08-19 10:38:56 -0600

    Add promises lesson part 2

--- 01-bases/src/bases/08-promises.ts
+++ 01-bases/src/bases/08-promises.ts
@@ -1,18 +1,36 @@
-console.log('Inicio');
+// console.log('Inicio');
 
-new Promise(( resolve, reject )=> {
-    //console.log('Cuerpo de la promesa');
-    setTimeout(() => {
-        // resolve('Mi amigo cumplió');
-        // console.log('Hola');
-        // console.log('Hola');
+import type { Hero } from "../data/heroes";
+import { getHeroById } from "./07-imp-exp";
 
-        reject('Mi amigo no cumplió');
+// new Promise(( resolve, reject )=> {
+//     //console.log('Cuerpo de la promesa');
+//     setTimeout(() => {
+//         // resolve('Mi amigo cumplió');
+//         // console.log('Hola');
+//         // console.log('Hola');
+
+//         reject('Mi amigo no cumplió');
                 
-    }, 1000);
+//     }, 1000);
     
-}).then((message) => console.log( message ))
-.catch( errorMessage => console.log(errorMessage))
-.finally(() => console.log('Fin de la promesa'));
+// }).then((message) => console.log( message ))
+// .catch( errorMessage => console.log(errorMessage))
+// .finally(() => console.log('Fin de la promesa'));
 
-console.log('Fin');
+// console.log('Fin');
+
+const getHeroByIdAsync = (id: number): Promise<Hero> => {
+    return new Promise( (resolve, reject) => {
+        setTimeout(()=> {
+            const hero = getHeroById(id);
+            hero
+                ? resolve(hero)
+                : reject(`Heroe no encontrado #${id}`);
+        }, 1500)
+    });
+};
+
+getHeroByIdAsync(2)
+    .then(hero=> console.log('El nombre es', hero.name))
+    .catch(message => alert(message));
