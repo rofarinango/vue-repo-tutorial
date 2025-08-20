d94c4d9437b59cb6136eacfba2339c9f57cf0828
Rodrigo Farinango <rodrigofarii@gmail.com>
2025-08-20 12:08:35 -0600

    Add v-if and v-show lesson

--- 02-intro-vue/intro/app.js
+++ 02-intro-vue/intro/app.js
@@ -1,37 +1,27 @@
+const quotes = [
+    { quote: 'The night is darkest just before the dawn. And I promise you, the dawn is coming.', author: 'Harvey Dent, The Dark Knight' },
+    { quote: 'I believe what doesn’t kill you simply makes you, stranger.', author: 'The Joker, The Dark Knight' },
+    { quote: 'Your anger gives you great power. But if you let it, it will destroy you… As it almost did me', author: 'Henri Ducard, Batman Begins' },
+    { quote: 'You either die a hero or live long enough to see yourself become the villain.', author: 'Harvey Dent, The Dark Knight' },
+    { quote: 'If you’re good at something, never do it for free.', author: 'The Joker, The Dark Knight' },
+    { quote: 'Yes, father. I shall become a bat.', author: 'Bruce Wayne/Batman, Batman: Year One' },
+]
+
 const { createApp, ref } = Vue;
 
 const app = createApp({
-    // template: `
-    // <h1>{{ message }}</h1>
-    // <p>- {{ author }} </p>
-    // `,
-
     setup() {
-        console.log("Hola desde setup");
-        const message = ref("Soy Batman");
-        // const author
-        // Bruce Wayne
-
-        const author = ref("Bruce Wayne");
-
-        const changeQuote = () => {
-            message.value = "Hola, soy Goku";
-            author.value = "Goku"
+        const showAuthor = ref(false);
+        const toggleAuthor = () => {
+            showAuthor.value = !showAuthor.value;
         }
 
-        // setTimeout(()=>{
-        //     message.value = "Soy Goku";
-        //     author.value = "Goku"
-        //     console.log("Esto se ha ejectuado");
-            
-        // }, 1000)
-
         return {
-            message, 
-            author,
-            changeQuote,
-        };
+            quotes,
+            showAuthor,
+            toggleAuthor
+        }
     }
 });
 
-app.mount("#myApp")
+app.mount("#myApp");
