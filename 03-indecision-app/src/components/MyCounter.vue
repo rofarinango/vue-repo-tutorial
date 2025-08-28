22565ea7595b1852b776d188e39714f63415901a
Rodrigo Farinango <rodrigofarii@gmail.com>
2025-08-28 11:31:21 -0600

    Add MyCounter single component

---
+++ 03-indecision-app/src/components/MyCounter.vue
@@ -0,0 +1,17 @@
+<template>
+  <section>
+    <h3>Counter: {{ counter }}</h3>
+    <h3>Square: {{ squareCounter }}</h3>
+
+    <div>
+      <button @click="counter++">+1</button>
+      <button @click="counter--">-1</button>
+    </div>
+  </section>
+</template>
+<script lang="ts" setup>
+import { computed, ref } from 'vue';
+
+const counter = ref(2);
+const squareCounter = computed(() => counter.value * counter.value);
+</script>
