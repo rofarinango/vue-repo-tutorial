4bc39c86b279384a25f2f7535125506a7934ade5
Rodrigo Farinango <rodrigofarii@gmail.com>
2025-09-02 12:41:04 -0600

    Add isolate SFC logic

--- 03-indecision-app/src/App.vue
+++ 03-indecision-app/src/App.vue
@@ -3,8 +3,10 @@
   <hr />
   <MyCounter :value="5" />
   <MyCounterScript :value="5" />
+  <MyCounterScript2 :value="5" />
 </template>
 <script lang="ts" setup>
+import MyCounterScript2 from './components/my-counter-script/MyCounterScript2.vue';
 import MyCounter from './components/MyCounter.vue';
 import MyCounterScript from './components/MyCounterScript.vue';
 </script>
