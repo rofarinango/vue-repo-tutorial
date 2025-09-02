eaf80ed6e334adc2102f1018ef4154c557aaacbf
Rodrigo Farinango <rodrigofarii@gmail.com>
2025-09-02 12:33:57 -0600

    Add Componente tradicional

--- 03-indecision-app/src/App.vue
+++ 03-indecision-app/src/App.vue
@@ -2,7 +2,9 @@
   <h1>My primera app</h1>
   <hr />
   <MyCounter :value="5" />
+  <MyCounterScript :value="5" />
 </template>
 <script lang="ts" setup>
 import MyCounter from './components/MyCounter.vue';
+import MyCounterScript from './components/MyCounterScript.vue';
 </script>
