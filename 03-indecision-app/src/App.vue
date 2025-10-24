784035773c0c1b1d7c9d46591dee80b3c0113465
Rodrigo Farinango <rodrigofarii@gmail.com>
2025-10-24 14:41:12 -0600

    Add Chat component

--- 03-indecision-app/src/App.vue
+++ 03-indecision-app/src/App.vue
@@ -1,12 +1,6 @@
 <template>
-  <h1 class="text-3xl font-bold underline">Hello world!</h1>
-  <hr />
-  <MyCounter :value="5" />
-  <MyCounterScript :value="5" />
-  <MyCounterScript2 :value="5" />
+  <IndecisionView></IndecisionView>
 </template>
 <script lang="ts" setup>
-import MyCounterScript2 from './components/my-counter-script/MyCounterScript2.vue';
-import MyCounter from './components/MyCounter.vue';
-import MyCounterScript from './components/MyCounterScript.vue';
+import IndecisionView from '@/views/IndecisionView.vue';
 </script>
---
+++ 03-indecision-app/src/components/chat/ChatMessages.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import ChatBubble from './ChatBubble.vue';
+</script>
+
+<template>
+  <div class="flex-1 overflow-y-auto p-4">
+    <div class="flex flex-col space-y-2">
+      <!-- Messages go here -->
+      <ChatBubble :its-mine="true" message="Hola Mundo" />
+      <ChatBubble
+        :its-mine="false"
+        message="no"
+        image="https://yesno.wtf/assets/no/14-cb78bf7104f848794808d61b9cd83eba.gif"
+      />
+    </div>
+  </div>
+</template>
